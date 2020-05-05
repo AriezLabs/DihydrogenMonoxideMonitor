2d54102f3f7af3b1397357c0f664b09d3393e524
Ariez <dpape39@icloud.com>
2020-05-05 22:22:37 +0200

    wip broken button + anim (i have no clue what i am doing)

--- DihydrogenMonoxideMonitor/ContentView.swift
+++ DihydrogenMonoxideMonitor/ContentView.swift
@@ -46,33 +46,83 @@ private var repeatingAnimation: Animation {
         .repeatForever()
 }
 
-struct ContentView: View {
-
+struct WavesBg: View {
     // magic numbers galore
-    @State private var backgroundOffsetY: CGFloat = 75
-    @State private var backgroundOffsetX: CGFloat = -20
-    @State private var foregroundOffsetY: CGFloat = 65
-    @State private var foregroundOffsetX: CGFloat = -110
+    @State private var backgroundOffsetY1: CGFloat = 75
+    @State private var backgroundOffsetY2: CGFloat = 60
+    
+    @State private var backgroundOffsetX1: CGFloat = -20
+    @State private var backgroundOffsetX2: CGFloat = 10
+    
+    @State private var foregroundOffsetY1: CGFloat = 65
+    @State private var foregroundOffsetY2: CGFloat = 75
+    
+    @State private var foregroundOffsetX1: CGFloat = -90
+    @State private var foregroundOffsetX2: CGFloat = -115
     
     var body: some View {
         return ZStack {
-            Wave(graphWidth: 1, amplitude: 0.025, offset: 0)
-                .foregroundColor(Color(red: 0, green: 0.68, blue: 0.85))
-                .offset(x: backgroundOffsetX, y: backgroundOffsetY)
-                .onAppear {
-                        self.backgroundOffsetY = 70
-                        self.backgroundOffsetX = 0
-                }
+                Wave(graphWidth: 1, amplitude: 0.1, offset: 0)
+                    .foregroundColor(Color(red: 0, green: 0.68, blue: 0.85))
+                    .offset(x: backgroundOffsetX1, y: backgroundOffsetY1)
+                    .onAppear {
+                        self.backgroundOffsetY1 = self.backgroundOffsetY2
+                        self.backgroundOffsetX1 = self.backgroundOffsetX2
+                    }.frame(width: UIScreen.main.bounds.size.width, height: 200)
+                    
+                Wave(graphWidth: 1, amplitude: 0.15, offset: 0)
+                    .foregroundColor(Color(red: 0, green: 0.8, blue: 1))
+                    .offset(x: foregroundOffsetX1, y: foregroundOffsetY1)
+                    .onAppear {
+                        self.foregroundOffsetY1 = self.foregroundOffsetY2
+                        self.foregroundOffsetX1 = self.foregroundOffsetX2
+                }.frame(width: UIScreen.main.bounds.size.width, height: 200)
                 
-            Wave(graphWidth: 1, amplitude: 0.03, offset: 0)
-                .foregroundColor(Color(red: 0, green: 0.8, blue: 1))
-                .offset(x: foregroundOffsetX, y: foregroundOffsetY)
-                .onAppear {
-                        self.foregroundOffsetY = 70
-                        self.foregroundOffsetX = -75
-                }
+            }.animation(repeatingAnimation)
+        }
+}
+
+struct DrinkButton: View {
+    var text: String
+    var action: () -> Void
+    var body: some View {
+        Button(action: {
+            withAnimation { self.action() }
+        }) {
+            HStack(spacing: 20) {
+                Image(systemName: "plus.circle.fill")
+                .resizable()
+                .frame(width: 30, height: 30)
+                Text(text)
+                .font(.system(size: 30, design: .rounded))
+            }
+            .accentColor(Color.white.opacity(0.8))
+        }
+        .frame(width: 180, height: 70)
+        .background(Color.black.opacity(0.5))
+        .cornerRadius(420.69)
+    }
+}
+
+struct ContentView: View {
+    @State private var drank: CGFloat = 0
+    var step: CGFloat = 10
+    
+    var body: some View {
+        return ZStack {
+            VStack {
+                WavesBg()
+                
+                Rectangle()
+                .frame(width: UIScreen.main.bounds.size.width, height: drank)
+                    .foregroundColor(Color(red: 0, green: 0.8, blue: 1))
+            }
             
-        }.animation(repeatingAnimation)
+            DrinkButton(text: String(Int(step)) + "ml") {
+                self.drank += CGFloat(self.step)
+                print("8=✊=D" + String(repeating: "💦", count: Int(self.drank/self.step)))
+            }
+        }
     }
 }
 
